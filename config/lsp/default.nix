40b1496d0d8f6fbc52d4f9bf4c8d8bffe0171ccc
Scott Cowe <scott.t.cowe@gmail.com>
2024-05-07 01:59:54 +0100

    added more lsps

--- config/lsp/default.nix
+++ config/lsp/default.nix
@@ -3,7 +3,32 @@
     enable = true;
 
     servers = {
+      # Config / scripting
       nil_ls.enable = true;
+      lua-ls.enable = true;
+      pylsp.enable = true;
+      jsonls.enable = true;
+      yamlls.enable = true;
+
+      # C
+      clangd.enable = true;
+     
+      # Rust
+      rust-analyzer = {
+        enable = true;
+        installCargo = true;
+        installRustc = true;
+      };
+
+      # JVM
+      java-language-server.enable = true;
+      kotlin-language-server.enable = true;
+      
+      # Web dev
+      html.enable = true;
+      cssls.enable = true;
+      tsserver.enable = true;
+      phpactor.enable = true;
     };
   };
 }
