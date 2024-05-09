5cb04bc14e59d20ab92567ac7b4ca73fa89f6e3f
Scott Cowe <scott.t.cowe@gmail.com>
2024-05-09 01:19:19 +0100

    updated cmp thing

--- config/cmp/default.nix
+++ config/cmp/default.nix
@@ -29,6 +29,12 @@
           function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
+            elseif luasnip.expandable() then
+              luasnip.expand()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif check_backspace() then
+              fallback()
             else
               fallback()
             end
